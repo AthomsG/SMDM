a39ba70f2e1d5afa0ceb4000350786d415b849eb
AthomsG <47532248+AthomsG@users.noreply.github.com>
2022-10-16 17:48:17 +0100

    added missing_data_plot.R and folder with output

--- code/import_data.R
+++ code/import_data.R
@@ -1,3 +1,7 @@
+# LIBRARIES
+#install.packages("ggplot2") # install the package
+library(ggplot2)
+
 data<-read.csv("datasets/MI.data") #Load Dataset
 
 dim(data) #Check dimention
@@ -5,3 +9,5 @@ dim(data) #Check dimention
 y<-data[c(122)] #122. Relapse of the myocardial infarction (REC_IM): Nominal Cases Fraction 
 
 data<-data[-c(95,102,105,122)] #Remove variables not useful for predition.
+
+data[]<-lapply(data, function(x) as.numeric(as.character(x))) #Force all datatypes to be numeric
---
+++ code/missing_data_plot.R
@@ -0,0 +1,27 @@
+# LIBRARIES
+#install.packages("ggplot2") # install the package
+library(ggplot2)
+
+data<-read.csv("datasets/MI.data") #Load Dataset
+
+dim(data) #Check dimention
+
+y<-data[c(122)] #122. Relapse of the myocardial infarction (REC_IM): Nominal Cases Fraction 
+
+data<-data[-c(95,102,105,122)] #Remove variables not useful for predition.
+
+data[]<-lapply(data, function(x) as.numeric(as.character(x))) #Force all datatypes to be numeric
+
+missing<-(colMeans(is.na(data)))*100 #Get percentage of missing values of each col
+missing = data.frame(as.list(missing))
+missing <- as.data.frame(t(missing))
+
+print(missing)
+
+row.names(missing)
+
+#Plot barplot with variables and percentage of missing values
+ggplot(missing, aes(x=row.names(missing), y=missing[, "V1"])) + 
+  geom_col(fill = "#0099f9") + coord_flip() + labs(x='Variable Name', y='Percentage Missing')
+
+ggsave(file="missing_data.pdf", width=4, height=12, dpi=300)
