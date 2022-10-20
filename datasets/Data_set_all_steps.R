3ba810724675e5ab9736c51f8241f496138bde3e
wesselgeerlings <wessel.geerlings@gmail.com>
2022-10-20 15:52:26 +0100

    Code to combine all pre-processing steps

---
+++ datasets/Data_set_all_steps.R
@@ -0,0 +1,25 @@
+#Steps in order to fix the dataset
+
+
+#Remove variables that are useless
+
+
+#Remove variables that have 80% missing values
+
+
+#Remove variables with correlation matrix (real variables)
+
+
+#Remove variables with Chi-Squared test (categorical variables)
+
+
+#Fix NaN values
+
+
+#Normalization of real values
+
+
+#One hot encoding of Ordinal values
+
+
+#
